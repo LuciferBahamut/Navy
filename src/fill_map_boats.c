76bf7c006f6ffdec6d2961b4d59e841de90def0e
Chauvin Chloé <Chloe@linux.home>
2020-02-22 14:24:31 +0100

    [FIX] fixed the position of the boat

--- src/fill_map_boats.c
+++ src/fill_map_boats.c
@@ -36,8 +36,8 @@ void fill_map_w_boats(map_t *m, char **pos)
 {
     for (int i = 2; i != 17; i++) {
         if (m->map[0][i] == pos[0][2])
-            m->map[catoi(pos[0][3]) + 2][i] = '2';
+            m->map[catoi(pos[0][3]) + 1][i] = '2';
         if (m->map[0][i] == pos[0][5])
-            m->map[catoi(pos[0][6]) + 2][i] = '2';
+            m->map[catoi(pos[0][6]) + 1][i] = '2';
     }
 }
