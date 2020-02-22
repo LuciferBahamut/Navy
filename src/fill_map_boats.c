692ab85c69db6d6197da2762ca914c6fbb39f1c2
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-02-22 13:44:21 +0100

    [UDP] fill_map_w_boat.c (fisrt boat)

--- src/fill_map_boats.c
+++ src/fill_map_boats.c
@@ -9,4 +9,10 @@
 
 void fill_map_w_boats(map_t *m, char **pos)
 {
+    for (int i = 2; i != 17; i++) {
+        if (m->map[0][i] == pos[0][2])
+            m->map[my_atoi(pos[0][3] + 2)][i] = '2';
+        if (m->map[0][i] == pos[0][5])
+            m->map[my_atoi(pos[0][6] + 2)][i] = '2';
+    }
 }
